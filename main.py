cda465b121289f8adc226ac41e9250d075651622
Егор <zztplq@icloud.com>
2022-12-24 18:14:33 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+def fir ():
+    print( "    /\")
+    print( "   /||\")
+    print("   /||||\")
+    print("  /||||||\")
+    print(" /||||||||\")
+    print("/||||||||||\")
+
+def main ():
+        fir()
+    
